e4d4bb4262393884c77fac09ac15316548882195
Bishal Sunuwar <bishalsunuwar24@gmail.com>
2023-10-17 23:27:21 +0545

    backend signup and login

---
+++ backend/routes/auth.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  postSignUp,
+  getAllUser,
+  getUser,
+  login,
+} = require("../controllers/signup");
+
+const { signup_validator, login_validator } = require("../middleware/user");
+
+router.get("/users", getAllUser);
+router.get("/users/:id", getUser);
+
+router.post("/auth/signup", signup_validator, postSignUp);
+router.post("/auth/login", login_validator, login);
